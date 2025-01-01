6e8cbfb940ca018319b763e4fc5155966376188d
arturfrimu <artur@tecorien.com>
2025-01-01 21:40:47 +0200

    Add Item entity and create item table

--- src/main/java/com/arturfrimu/firstdeploy/FirstDeployApplication.java
+++ src/main/java/com/arturfrimu/firstdeploy/FirstDeployApplication.java
@@ -1,10 +1,20 @@
 package com.arturfrimu.firstdeploy;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @RestController
 @CrossOrigin("*")
@@ -17,5 +27,19 @@ public class FirstDeployApplication {
     @GetMapping
     public String up() {
         return "I'm UP!";
+    }
+
+    @Entity
+    @Table(name = "item")
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Item {
+        @Id
+        @GeneratedValue(strategy = IDENTITY)
+        private Long id;
+        private String text;
     }
 }
